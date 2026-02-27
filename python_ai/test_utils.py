fe98db587b94941fbc993224fb78aa96b29c49ca
philip giovia <pjjr1019@gmail.com>
2026-02-27 13:26:11 -0500

    fix: strict E501 compliance for test_utils.py (variable split)

--- python_ai/test_utils.py
+++ python_ai/test_utils.py
@@ -92,7 +92,8 @@ def test_resource_monitor_main(monkeypatch):
         import python_ai.resource_monitor as rm
     except ModuleNotFoundError as e:
         if e.name == "psutil":
-            pytest.skip("psutil not installed; " "skipping resource_monitor test.")
+            skip_msg = "psutil not installed; " "skipping resource_monitor test."
+            pytest.skip(skip_msg)
         else:
             raise
     called = {}
